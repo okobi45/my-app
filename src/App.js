a897b97c4ea3d98586eb07f670ab6d68aed37fec
Okobi Eigbe <okobieigbe@gmail.com>
2022-04-08 01:27:12 +0100

    components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
-import {Navbar} from './components/Navbar';
+import {Navbar} from './components/Navbar'
+import {Landing} from './components/Landing'
 
 const App = ()=> {
   return (
     <>
       <Navbar/>
+      <Landing/>
       <div className='container'>
-        
+
       </div>
     </>
   );
---
+++ src/components/Landing.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Landing = () =>{
+    return(
+        <div className='hero'>
+            <h2>
+                Welcome to My React App
+            </h2>
+        </div>
+    )
+}
